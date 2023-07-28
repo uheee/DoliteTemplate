68501044354e3088ab1757de4fa9a116ee55d6e8
Snowind <jinks.tao@gmail.com>
2023-07-28 16:40:22 +0800

    feat($api.utils): add CRUD mapping extension methods

--- src/DoliteTemplate.Api/Utils/AutoMapperExtensions.cs
+++ src/DoliteTemplate.Api/Utils/AutoMapperExtensions.cs
@@ -9,4 +9,34 @@ public static class AutoMapperExtensions
     {
         expression.ForAllMembers(opts => opts.Condition((_, _, srcMember) => srcMember != null));
     }
+
+    public static void CreateCrudMap<TEntity, TReadDto, TCreateDto, TUpdateDto>(this Profile profile,
+        Action<IMappingExpression<TEntity, TReadDto>>? read = null,
+        Action<IMappingExpression<TCreateDto, TEntity>>? create = null,
+        Action<IMappingExpression<TUpdateDto, TEntity>>? update = null)
+    {
+        var readExpression = profile.CreateMap<TEntity, TReadDto>();
+        read?.Invoke(readExpression);
+        var createExpression = profile.CreateMap<TCreateDto, TEntity>();
+        create?.Invoke(createExpression);
+        var updateExpression = profile.CreateMap<TUpdateDto, TEntity>();
+        updateExpression.OnlyForNotNullProperties();
+        update?.Invoke(updateExpression);
+    }
+
+    public static void CreateCrudMap<TEntity, TReadDto, TCreateUpdateDto>(this Profile profile,
+        Action<IMappingExpression<TEntity, TReadDto>>? read = null,
+        Action<IMappingExpression<TCreateUpdateDto, TEntity>>? create = null,
+        Action<IMappingExpression<TCreateUpdateDto, TEntity>>? update = null)
+    {
+        CreateCrudMap<TEntity, TReadDto, TCreateUpdateDto, TCreateUpdateDto>(profile, read, create, update);
+    }
+
+    public static void CreateCrudMap<TEntity, TDto>(this Profile profile,
+        Action<IMappingExpression<TEntity, TDto>>? read = null,
+        Action<IMappingExpression<TDto, TEntity>>? create = null,
+        Action<IMappingExpression<TDto, TEntity>>? update = null)
+    {
+        CreateCrudMap<TEntity, TDto, TDto, TDto>(profile, read, create, update);
+    }
 }
