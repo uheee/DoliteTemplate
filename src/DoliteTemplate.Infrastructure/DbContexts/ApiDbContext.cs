da0531282aa9ebc3409bbed9452642e18f3094cf
Snowind <jinks.tao@gmail.com>
2024-06-23 17:12:52 +0800

    fix($infrastructure.dbcontexts): fix db context error

--- src/DoliteTemplate.Infrastructure/DbContexts/ApiDbContext.cs
+++ src/DoliteTemplate.Infrastructure/DbContexts/ApiDbContext.cs
@@ -1,11 +1,22 @@
+using DoliteTemplate.Domain.Entities;
 using DoliteTemplate.Infrastructure.DbContexts.Base;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DoliteTemplate.Infrastructure.DbContexts;
 
 public class ApiDbContext : BaseDbContext<ApiDbContext>
 {
+    #region Entites
+
+    public DbSet<Order> Orders { get; init; }
+
+    #endregion
+
+    #region Constructors
+
+    [ActivatorUtilitiesConstructor]
     public ApiDbContext(DbContextOptions<ApiDbContext> options) :
         base(options)
     {
@@ -15,4 +26,6 @@ public class ApiDbContext : BaseDbContext<ApiDbContext>
         base(options, httpContextAccessor)
     {
     }
+
+    #endregion
 }
